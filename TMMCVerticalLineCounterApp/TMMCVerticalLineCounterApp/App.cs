3d73af75ee4aa960e10f1f1c352f05e48384504d
sethclim <56656904+sethclim@users.noreply.github.com>
2026-04-07 17:04:14 -0400

    Setup App DI

---
+++ TMMCVerticalLineCounterApp/TMMCVerticalLineCounterApp/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMMCVerticalLineCounterApp
+{
+    internal class App
+    {
+        public App()
+        {
+            Console.WriteLine("Initializing App...");
+        }
+
+        public Task Run()
+        {
+            Console.WriteLine("Running App...");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- TMMCVerticalLineCounterApp/TMMCVerticalLineCounterApp/Program.cs
+++ TMMCVerticalLineCounterApp/TMMCVerticalLineCounterApp/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TMMCVerticalLineCounterApp;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {  
+        services.AddSingleton<App>();
+    })
+    .Build();
+
+var app = host.Services.GetRequiredService<App>();
+
+await app.Run();
+
